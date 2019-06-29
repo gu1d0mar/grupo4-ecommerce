4492ac058b72fa6cc7ebad570485b5d995855712
guidomar <guidomar17@gmail.com>
2019-06-29 00:15:36 -0300

    Commit de modificaciones en los <p> de "registro.php".

--- html/registro.php
+++ html/registro.php
@@ -5,32 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css\registro.css">
-    <title>Document</title>
+    <title>Registrate en Fernandez-Fernandez Coiffeur</title>
 </head>
 <body>
 <form action="action_page.php">
   <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
+    <h1>Creá tu cuenta</h1>
+    <p>Completá tus datos</p>
     <hr>
 
-    <label for="email"><b>Email</b></label>
+    <label for="email"><b>E-mail</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>
 
-    <label for="psw"><b>Password</b></label>
+    <label for="psw"><b>Clave</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <label for="psw-repeat"><b>Repita Clave</b></label>
     <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
     <hr>
 
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-    <button type="submit" class="registerbtn">Register</button>
+    <p>Al registrarme, declaro que soy mayor de edad y acepto los <a href="#">Términos y condiciones</a> de Fernandez-Fernandez Coiffure.</p>
+    <button type="submit" class="registerbtn">Crear cuenta</button>
   </div>
 
   <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
+    <p>Ya tienes cuenta? <a href="#">Ingresa aquí</a>.</p>
   </div>
 </form>
 </body>
 </html>
+
+  
