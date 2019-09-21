f2e86cad82836ad049bc28fe9edce9bb683ded48
Gianluca <gpinieri1@gmail.com>
2019-09-20 21:38:24 -0300

    Product, user and shops views

--- peluquin/routes/web.php
+++ peluquin/routes/web.php
@@ -15,9 +15,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Products
+Route::get('/products', 'ProductsController@directory')->name('products');
+Route::get('/products/search', 'productsController@search')->name('products.search');
+Route::delete('/products/delete', 'productsController@destroy')->name('products.destroy')//->middleware(admins);
+Route::get('/products/add','productsController@create')->name('products.create')//->middleware(admins);
+Route::post('/products/add','productsController@store')->name('products.save')//->middleware(admins);
+Route::get('/products/{id}', 'productsController@show')->name('products.show');
+Route::get('/products/{id}/edit', 'productsController@edit')->name('products.edit')//->middleware(admins);
+Route::put('/products/{id}', 'productsController@update')->name('products.update')//->middleware(admins);
+Route::get('/products/{id}/buy', 'productsController@buy')->name('products.buy')->middleware('auth');
+Route::post('/products/{id}/buy', 'productsController@addToCart')->name('products.cart')->middleware('auth');
+
+//Cart
 Route::get('/cart', function () {
-    return view('cart');
+  return view('cart');
 });
+
+//Users
+Route::delete('/user/delete', 'userController@destroy')->name('user.destroy')-middleware('auth');
+Route::get('/user{id}', 'userController@show')->name('user')-middleware('auth');
+Route::get('/user/{id}/edit', 'userController@edit')->name('user.edit')-middleware('auth');
+Route::put('/user/{id}', 'userController@update')->name('user.update')-middleware('auth');
+
+//Shops
+Route::get('/shops','shopsController@directory')->name('shops');
+Route::get('/shops/search','shopsController@search')->name('shops.search');
+Route::delete('/shops/delete', 'shopsController@destroy')->name('shop.destroy')//->middleware(admins);
+Route::get('/shops/{id}', 'shopsController@show')->name('shop.show');
+Route::get('/shops/{id}/edit', 'shopsController@edit')->name('shop.edit')//->middleware(admins);
+Route::put('/shops/{id}', 'shopsController@update')->name('shop.update')//->middleware(admins);
+
+
 
 Route::get('/contact', function () {
     return view('contact');
@@ -27,13 +56,6 @@ Route::get('/faq', function () {
     return view('faq');
 });
 
-Route::get('/product', function () {
-    return view('product');
-});
-
-Route::get('/user', function () {
-    return view('user');
-});
 
 Auth::routes();
 
