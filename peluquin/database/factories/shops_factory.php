9dd45c6313309773f022f28e36c221ae9ea5859e
guidomar <guidomar17@gmail.com>
2019-09-22 19:51:20 -0300

    Modified 'shops_factory.php' to successfully generate 'shops'

--- peluquin/database/factories/shops_factory.php
+++ peluquin/database/factories/shops_factory.php
@@ -7,11 +7,16 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Shop::class, function (Faker $faker) {
     return [
-        'email' => $faker->mail,
-        'name' =>  $faker->work,
-        'adress' => $faker->adress,
+        //'email' => $faker->mail,
+        //'name' =>  $faker->work,
+        //'adress' => $faker->adress,
+        //'work_hours' => $faker->time,
+        //'password' =>$faker->bcrypt('pass')
+        'email' => $faker->email,
+        'name' => $faker->company,
+        'address' => $faker->address,
         'work_hours' => $faker->time,
-        'password' =>$faker->bcrypt('pass')
+        'password' => $faker->password
 
     ];
 });
