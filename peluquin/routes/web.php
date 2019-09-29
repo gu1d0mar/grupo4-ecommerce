525a1276aa387d6b45dac9e612346af155ad6b65
Gianluca <gpinieri1@gmail.com>
2019-09-28 22:31:47 -0300

    Fixed cart and header views

--- peluquin/routes/web.php
+++ peluquin/routes/web.php
@@ -12,38 +12,21 @@
 */
 
 Route::get('/', 'HomeController@index')->name('home');
-
-//Products
-//Route::get('/products', 'ProductsController@directory')->name('products');
-//Route::get('/products/search', 'productsController@search')->name('products.search');
-//Route::delete('/products/delete', 'productsController@destroy')->name('products.destroy')//->middleware(admins);
-//Route::get('/products/add','productsController@create')->name('products.create')//->middleware(admins);
-//Route::post('/products/add','productsController@store')->name('products.save')//->middleware(admins);
-//Route::get('/products/{id}', 'productsController@show')->name('products.show');
-//Route::get('/products/{id}/edit', 'productsController@edit')->name('products.edit')//->middleware(admins);
-//Route::put('/products/{id}', 'productsController@update')->name('products.update')//->middleware(admins);
-//Route::get('/products/{id}/buy', 'productsController@buy')->name('products.buy')->middleware('auth');
-//Route::post('/products/{id}/buy', 'productsController@addToCart')->name('products.cart')->middleware('auth');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/shops','ShopsController@directory')->name('shops');
 Route::get('/shops/search', 'ShopsController@search')->name('shops.search');
 Route::get('/shops/{id}','ShopsController@show')->name('shops.show');
 
-//Cart
-Route::get('/cart', function () {
-  return view('cart');
-})->name('cart');
-
-Route::get('/cart/add/{id}',                           'CartController@add')->middleware('auth');
-Route::get('/cart/remove/{id}',                        'CartController@remove')->middleware('auth');
+Route::get('/cart','CartController@show')->name('cart')->middleware('auth');
+Route::get('/cart/add/{id}','CartController@add')->middleware('auth');
+Route::get('/cart/remove/{id}','CartController@remove')->middleware('auth');
 
 //Users
 //Route::delete('/user/delete', 'userController@destroy')->name('user.destroy')-middleware('auth');
 Route::get('/user','UsersController@show')->name('user.profile')->middleware('auth');
 Route::get('/user/config','UsersController@edit')->name('user.edit')->middleware('auth');
 //Route::put('/user/{id}', 'userController@update')->name('user.update')-middleware('auth');
-
-
 
 
 Route::get('/contact', function () {
