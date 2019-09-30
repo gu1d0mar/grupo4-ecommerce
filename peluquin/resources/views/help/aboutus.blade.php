f6e545f29d8ae0b9c607e2fd7aa1a668a323fee6
guidomar <guidomar17@gmail.com>
2019-09-30 15:19:07 -0300

    Changed faq.blade.php. Added aboutus.blade.php. Backed up Guido's web.php into web.php.guido

---
+++ peluquin/resources/views/help/aboutus.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('title') Quienes Somos? @endsection
+@section('content')
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        Versión vigente 12/08/2019
+      </div>
+      <div class="card-body">
+        <h2 class="card-title">Quienes Somos?</h3>
+        <h4 class="card-title">01 - Quienes Somos?</h3>
+        <p class="card-text text-muted">Somos una PyME dedicada a facilitar el contacto entre Proveedores de Servicios y sus Clientes. Compartiendo nuestra plataforma de reserva de servicios para facilitar dicha misión.</p>
+        <h4 class="card-title">02 - Nuestra historia </h4>
+        <p class="card-text text-muted"> Surgidos a principios del 2019. Somos un grupo de estudiantes de programación de Digital House. Siendo nuestro objetivo construir un Market Place para facilitar el agendado y contratación de productos y servicios en general y de la industria estética en particular.</p>
+        <h4 class="card-title">03 - Como planeamos hacerlo?</h4>
+        <p class="card-text text-muted">Para cumplir con nuestros objetivos decidimos sumar una plataforma más al saturado mercado de la reserva de turnos on-line.</p>
+      </div>
+    </div>
+  </div>
+
+@endsection
--- peluquin/resources/views/help/faq.blade.php
+++ peluquin/resources/views/help/faq.blade.php
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>FAQ</title>
-  </head>
-  <body>
-    <h1>FAQ</h1>
-  </body>
-</html>
+@extends('layouts.app')
+@section('title') Preguntas Frecuentes @endsection
+@section('content')
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        Versión vigente 12/08/2019
+      </div>
+      <div class="card-body">
+        <h2 class="card-title">Preguntas Frequentes</h3>
+        <h4 class="card-title">01 - Quienes Somos?</h3>
+        <p class="card-text text-muted">Somos una PyME dedicada a facilitar el contacto entre Proveedores de Servicios y sus Clientes. Compartiendo nuestra plataforma de reserva de servicios para facilitar dicha misión.</p>
+        <h4 class="card-title">02 - Puedo Registrame en su Plataforma? </h4>
+        <p class="card-text text-muted"> Todos son libres de registrarse y usar nuestra plataforma. Siempre y cuando cumplan con nuestros <a href="http://127.0.0.1:8000/termsandconditions">Terminos y condiciones</a>.</p>
+        <h4 class="card-title">03 - Que servicios puedo ofrecer en su plataforma? </h4>
+        <p class="card-text text-muted"> Ustede es libre de ofrecer cualquier tipo de producto o servicio. Siempre y cuando cumpla con las normativas aplicables a dichos productos y servicios en el lugar donde se consuma, venda y produzca el producto o servicio. Así como si dicho producto o servicio no fuera en contra de nuestros <a href="http://127.0.0.1:8000/termsandconditions">Terminos y condiciones</a> o de los usos y costumbres del lugar donde tendrá lugar el servicio o transacción.</p>
+        <h4 class="card-title">04 - Que protección me ofrecen en caso de daño causado por el servicio o por un producto defectuoso?</h4>
+        <p class="card-text text-muted"> La empresa se compromete a investigar toda denuncia recibida al respecto de un proveedor que haga uso de nuestra plataforma. El resarcimiento de daños o perjuicios corre a cargo del proveedor. Siendo la plataform y la empresa solo un intermediario entre partes.</p>
+        <h4 class="card-title">05 - Que responsibilidades me competen como usuario del servicio?</h4>
+        <p class="card-text text-muted">Su única responsabildiad es proveer un crítica constructiva, sin lenguage difamatorio u ofensivo, al momento de calificar un servicio. Así como respetar los terminos contratados una vez adquirido un producto o servicio. Ni el comprador ni el proveedor está autorizados a modificar el producto o servicio adquiridos luego de contratados en la plataforma.</p>
+      </div>
+    </div>
+  </div>
+
+@endsection
